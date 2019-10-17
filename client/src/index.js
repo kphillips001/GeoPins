c972fb8062032c1f42e2fcf32a70737ca11a2eef
Kevin <knphillips01@gmail.com>
2019-10-17 12:46:53 -0500

    set up client and google oauth

---
+++ client/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import App from "./pages/App";
+import Splash from "./pages/Splash";
+
+import "mapbox-gl/dist/mapbox-gl.css";
+
+const Root = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Splash} />
+      </Switch>
+    </Router>
+  );
+};
+
+ReactDOM.render(<Root />, document.getElementById('root'));
+
+
