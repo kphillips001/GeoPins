db13f2c92c625d1bd30a5c99d4c030621162327d
Kevin <knphillips01@gmail.com>
2019-10-17 21:03:41 -0500

    added useContext and useReducer hooks

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useContext, useReducer} from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 import App from "./pages/App";
 import Splash from "./pages/Splash";
+import Context from './context';
+import reducer from './reducer';
 
 import "mapbox-gl/dist/mapbox-gl.css";
 
 const Root = () => {
+  const initialState = useContext(Context);
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  console.log({ state }); 
+  
   return (
     <Router>
+      <Context.Provider value={{ state, dispatch }}>
       <Switch>
         <Route exact path="/" component={App} />
         <Route path="/login" component={Splash} />
       </Switch>
+      </Context.Provider>
     </Router>
   );
 };
