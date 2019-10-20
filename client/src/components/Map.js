f21438ef5166e282c1ad4ce9e6f586f5fe299661
Kevin <knphillips01@gmail.com>
2019-10-20 15:19:01 -0500

    highligted newly created pins

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import ReactMapGL, { NavigationControl, Marker } from 'react-map-gl';
 import { withStyles } from "@material-ui/core/styles";
+import differenceInMinutes from "date-fns/differenceInMinutes";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
@@ -56,6 +57,13 @@ const Map = ({ classes }) => {
     });
   };
 
+  const highlightNewPin = pin => {
+    const isNewPin =
+      differenceInMinutes(Date.now(), Number(pin.createdAt)) <= 30;
+    return isNewPin ? "limegreen" : "darkblue";
+  };
+
+
   return (
   <div className={classes.root}>
     <ReactMapGL
@@ -110,7 +118,7 @@ const Map = ({ classes }) => {
       offsetTop={-37}
     
   >
-    <PinIcon size={40} color="darkblue" />
+    <PinIcon size={40} color={highlightNewPin(pin)} />
   </Marker>
     ))}
     </ReactMapGL>
