26d5534f76ad7923792c836455a67790fcee71ab
Kevin <knphillips01@gmail.com>
2019-10-21 13:08:22 -0500

    added ability to create comments on users posts

--- client/src/components/Comment/Comments.js
+++ client/src/components/Comment/Comments.js
@@ -25,7 +25,6 @@ const Comments = ({ comments, classes }) => (
               >
                 {comment.author.name}
               </Typography>{" "}
-             
             </>
           }
         />
